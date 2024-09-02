608d7a1f7116a6f040bee3d2b5532151f0e4c420
ms_abhijith <122276182+AbhijithSatheesan@users.noreply.github.com>
2024-09-02 16:44:57 +0530

    replaced proxy with domain

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-const BACKEND_URL = 'https://itsmeabhijith.shop';
+// const BACKEND_URL = 'https://itsmeabhijith.shop';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: `${BACKEND_URL}/api`,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      '/images': {
-        target: BACKEND_URL,
-        changeOrigin: true,
-      },
-    },
-  },
-});
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: `${BACKEND_URL}/api`,
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       },
+//       '/images': {
+//         target: BACKEND_URL,
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
 
 
 
@@ -26,23 +26,23 @@ export default defineConfig({
 
 
 // for development
-// // vite.config.js
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'http://127.0.0.1:8000/api',
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ''),
-//       },
-//       '/images': {
-//         target: 'http://127.0.0.1:8000/',
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-// });
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://itsmeabhijith.shop',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/images': {
+        target: 'https://itsmeabhijith.shop',
+        changeOrigin: true,
+      },
+    },
+  },
+});
