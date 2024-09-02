08fe5c6f4323ab3c1ff648d0d3793a10b595e2e3
ms_abhijith <122276182+AbhijithSatheesan@users.noreply.github.com>
2024-09-02 16:05:15 +0530

    config edited

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,48 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+const BACKEND_URL = 'https://itsmeabhijith.shop';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000/api',
+        target: `${BACKEND_URL}/api`,
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
       '/images': {
-        target: 'http://127.0.0.1:8000/',
+        target: BACKEND_URL,
         changeOrigin: true,
       },
     },
   },
 });
+
+
+
+
+
+
+// for development
+// // vite.config.js
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://127.0.0.1:8000/api',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       },
+//       '/images': {
+//         target: 'http://127.0.0.1:8000/',
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
