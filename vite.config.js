3aa0e1b70513a7a060fb52ad7a2c02f3be3704a8
ms_abhijith <122276182+AbhijithSatheesan@users.noreply.github.com>
2024-10-05 14:44:48 +0530

    added .env variables for vercel

--- vite.config.js
+++ vite.config.js
@@ -86,32 +86,60 @@
 
 
 
+// http://127.0.0.1:8000/
 
 
 
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// const BACKEND_URL = 'http://127.0.0.1:8000/';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: `${BACKEND_URL}/api`,
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       },
+//       '/images': {
+//         target: BACKEND_URL,
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
+
+
+
+
+
+// https://itsmeabhijith.shop/
+
+
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const BACKEND_URL = 'https://itsmeabhijith.shop/';
+export default defineConfig(({ mode }) => {
+  // Load env file based on the mode ('development' or 'production')
+  const env = loadEnv(mode, process.cwd());
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: `${BACKEND_URL}/api`,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      '/images': {
-        target: BACKEND_URL,
-        changeOrigin: true,
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: `${env.VITE_API_URL}/api`,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+        '/images': {
+          target: env.VITE_IMAGE_URL,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
-
-
-
-
-
