1e8838b9dbead49da6ed689b3ae0ce3516e9b02a
ms_abhijith <122276182+AbhijithSatheesan@users.noreply.github.com>
2024-10-09 14:11:31 +0530

    categories with api.js corrected again

--- src/screens/categories/Categories.jsx
+++ src/screens/categories/Categories.jsx
@@ -12,7 +12,7 @@ function Categories() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await api.get('/api/colleges/categories'); // Using the axios instance
+        const response = await api.get('api/colleges/categories'); // Using the axios instance
         setData(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
