70f61c42a33ec2b6c41753c657f22b5c718ce49b
ms_abhijith <122276182+AbhijithSatheesan@users.noreply.github.com>
2024-10-11 18:15:53 +0530

    baseurl for images

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const isProduction = true; // Hardcoded as true for production
-const productionBaseURL = 'http://127.0.0.1:8000/'; // Your production API URL
+const productionBaseURL = 'https://itsmeabhijith.shop/'; // Your production API URL
 
 export const getFullURL = (path) => {
   return isProduction ? `${productionBaseURL}${path}` : path;
